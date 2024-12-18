8086422db607cd38555046fe7ce15c2a899fb568
BlackTeaCFG <blackteacfg@gmail.com>
2024-12-18 16:47:54 +0300

    init commit

---
+++ src/calculator.py
@@ -0,0 +1,38 @@
+def add(a: float, b: float) -> float:
+    """
+    Возвращает сумму двух чисел.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Сумма чисел a и b.
+    """
+    return a + b
+
+def multiply(a: float, b: float) -> float:
+    """
+    Возвращает произведение двух чисел.
+
+    Args:
+        a (float): Первое число.
+        b (float): Второе число.
+
+    Returns:
+        float: Произведение чисел a и b.
+    """
+    return a * b
+
+def subtract(a: float, b: float) -> float:
+    """
+    Возвращает разность двух чисел.
+
+    Args:
+        a (float): Уменьшаемое.
+        b (float): Вычитаемое.
+
+    Returns:
+        float: Результат вычитания b из a.
+    """
+    return a - b
